b1b4037fbe20a7f2f908edfdcb8eb14e17f502b2
TeerapatMan <teerapat.man19@hotmail.com>
2021-10-04 15:06:39 +0700

    adjust responsive ui

--- src/myFaculty.js
+++ src/myFaculty.js
@@ -1,73 +1,85 @@
-import React from 'react';
-import './App.css';
-import facultiesInfo from './data/faculties';
-import Profile from './Profile';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import React from "react";
+import "./App.css";
+import facultiesInfo from "./data/faculties";
+import Profile from "./Profile";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faSearch } from "@fortawesome/free-solid-svg-icons";
 
-export default class AutoCompletedText extends React.Component{
+export default class AutoCompletedText extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      suggestions: [],
+      text: "",
+      showProfile: false,
+      selectedFaculty: {},
+    };
+  }
 
-    constructor(props){
-        super(props)
-        this.state = {
-            suggestions: [],
-            text: '',
-            showProfile: false,
-            selectedFaculty: {}
-        }
+  onTextChange = (e) => {
+    const value = e.target.value;
+    let suggestions = [];
+    if (value.length > 0) {
+      const regex = new RegExp(`^${value}`, "i");
+      suggestions = facultiesInfo.sort().filter((v) => regex.test(v.FACULTY));
+      // suggestions = suggestions.map((value) => value.FACULTY);
     }
 
-    onTextChange = (e) => {
-        const value = e.target.value;
-        let suggestions = [];
-        if(value.length > 0){
-            const regex = new RegExp(`^${value}`, 'i');
-            suggestions = facultiesInfo.sort().filter(v => regex.test(v.FACULTY));
-            // suggestions = suggestions.map((value) => value.FACULTY);
-        }
+    this.setState(() => ({
+      suggestions,
+      text: value,
+      showProfile: false,
+    }));
+  };
 
-        this.setState(() => ({
-            suggestions,
-            text: value,
-            showProfile: false
-        }))
+  selectedText(value) {
+    this.setState(() => ({
+      text: value.FACULTY,
+      suggestions: [],
+      showProfile: true,
+      selectedFaculty: value,
+    }));
+  }
+
+  renderSuggestions = () => {
+    let { suggestions } = this.state;
+    if (suggestions.length === 0) {
+      return null;
     }
+    return (
+      <ul>
+        {suggestions.map((item, index) => (
+          <li key={index} onClick={() => this.selectedText(item)}>
+            {item.FACULTY}
+          </li>
+        ))}
+      </ul>
+    );
+  };
 
-    selectedText(value) {
-        this.setState(() => ({
-            text: value.FACULTY,
-            suggestions: [],
-            showProfile: true,
-            selectedFaculty: value
-        }));
-
-    }
-
-    renderSuggestions = () => {
-        let { suggestions } = this.state;
-        if(suggestions.length === 0){
-            return null;
-        }
-        return (
-            <ul >
-                {
-                    suggestions.map((item, index) => (<li key={index} onClick={() => this.selectedText(item)}>{item.FACULTY}</li>))
-                }
-            </ul>
-        );
-    }
-    
-    render() {
-        const { text, suggestions } = this.state;
-        return(
-            <div id="notebooks">
-                <h2><FontAwesomeIcon icon={faSearch} /> Search Your Faculty</h2>
-                <input id="query" type="text" onChange={this.onTextChange} value={text}/>
-                {this.renderSuggestions()}
-                {this.state.showProfile ? <Profile {...this.state.selectedFaculty}/> : <div />}
-                <span>Suggestions: {suggestions.length}</span>
-            </div>
-        );
-    }
-
+  render() {
+    const { text, suggestions } = this.state;
+    return (
+      <div id="wrapper">
+        <div id="notebooks">
+          <h2>
+            <FontAwesomeIcon icon={faSearch} /> Search Your Faculty
+          </h2>
+          <input
+            id="query"
+            type="text"
+            onChange={this.onTextChange}
+            value={text}
+          />
+          {this.renderSuggestions()}
+          {this.state.showProfile ? (
+            <Profile {...this.state.selectedFaculty} />
+          ) : (
+            <div />
+          )}
+          <span>Suggestions: {suggestions.length}</span>
+        </div>
+      </div>
+    );
+  }
 }
